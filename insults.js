1960ac61cc7637b580901b8b4afb2f90ec3bf182
Michael Gale <michael.gale@outlook.com.au>
2019-05-05 20:19:41 +1000

    add all important insults file

---
+++ insults.js
@@ -0,0 +1,9 @@
+var insult = [
+    "A pleasure to be teeing off with you again, sir.",
+    "Might I suggest a putter?",
+    "Pffft, five iron..",
+    "Very good sir, but personally I prefer golf.",
+    "To some golfers, the greatest handicap is the ability to add correctly."
+];
+
+console.log(insult[1]);
